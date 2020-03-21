285c531333c85e698758ef9c53e4c1be3fc6e8ae
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-03-21 12:34:42 +0800

    AE401-2
    分數等級

---
+++ b.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 12:14:18 2020
+
+@author: HARRY LIU
+"""
+a=input("請輸入你的成績")
+a=int(a)
+if a>=90:
+ print("B")
+ elif a<90 and a>80:
+ print("c")
+elif a<80 and a>70:
+    print("d")
+
+else:
+ print("不及格")
+
+
