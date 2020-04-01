4a6c4b50abe0090039ca67ea6fcaff712853ad72
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-04-01 20:30:17 +0800

    成績好不好
    看你的成績好不好

---
+++ D.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 20:15:58 2020
+
+@author: HARRY LIU
+"""
+
+
+
+ score=input("請輸入你的英成績:")
+ integer=input("請輸入你的數成績:")
+ score=int(score)
+ if score>= and integer>=90:
+       print("很好")
+elif score<60 and integer<60 :
+      print("爛")
+elif score<60 or score<60:
+     print("加油")
+elif:print("差點滿分")
