acd9e3c940a9795b3d8c5ba128ed5bf39fab734c
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-04-25 12:07:15 +0800

    Add files via upload

---
+++ 烏龜.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 10:55:17 2020
+
+@author: HARRY LIU
+"""
+import turtle
+
+A = turtle.Turtle()
+c=turtle. Screen()
+c.bgcolor("black")
+A.color("yellow")
+A.shape("turtle")
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+turtle.done
