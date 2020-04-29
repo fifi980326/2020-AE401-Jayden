c9f3a7a16cbde4465b13be2e906b2ac513c514d5
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-04-29 19:43:23 +0800

    畫圓
    烏龜畫圓

---
+++ u.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 19:20:19 2020
+
+@author: HARRY LIU
+"""
+
+
+
+import turtle
+
+A = turtle.Turtle()
+c=turtle. Screen()
+c.bgcolor("black")
+A.color("yellow")
+A.shape("turtle")
+A.pensize(6)
+
+i=0
+
+while i<8:
+   
+   A.left(45)
+   A.forward(100)
+   i=i+1
+turtle.done
