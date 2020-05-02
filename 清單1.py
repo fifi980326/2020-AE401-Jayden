03d32b1c7f62bc1814cc25ff1a31227e8139eb59
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-05-02 12:03:34 +0800

    清單

---
+++ 清單1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 11:34:27 2020
+
+@author: HARRY LIU
+"""
+scores=["羽球","桌球","籃球","跑步"]
+print(scores)
+scores.append("球場")
+print(scores)
+scores.insert(2,1)
+print(scores)
+scores.remove("球場")
+print(scores)
