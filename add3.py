debfd789319be23cf1a6c054ce930e02cab5aabf
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-05-23 12:15:00 +0800

    Add files via upload

---
+++ add3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 11:27:23 2020
+
+@author: HARRY LIU
+"""
+def add3(num1,num2,num3):
+    total=num1+num2+num3
+    return  total
+A=add3(1,2,3)
+print(A)
+
