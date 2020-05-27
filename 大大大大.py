c036ea8e0584b229f62f3c2bd57aa2a79aa5683c
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-05-27 20:14:02 +0800

    Add files via upload

---
+++ 大大大大.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 20:11:58 2020
+
+@author: HARRY LIU
+"""
+
+
+a=[]
+b=[]
+c=[]
+
+
+
+
+
+
+def f(ani1,ani2,ani3):
+    total=ani1+ani2+ani3
+    return total
+
+if a:
+    a=f("小狗","玩","球")
+    print(a)
+elif b:
+    b=f("貓","踢","玩具")
+    print(b)
+else c:
+    c=f("兔","咬","襪子")
+    print(c)
+   
+
+
+
+
+
+print(a) or print(b) or print(c)
