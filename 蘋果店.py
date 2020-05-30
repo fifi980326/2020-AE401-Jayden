f3ce6ec3a98975eccc3b9f35456b140e094c2670
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-05-30 12:32:42 +0800

    Add files via upload

---
+++ 蘋果店.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 11:22:22 2020
+
+@author: HARRY LIU
+"""
+while True:
+    print("-------蘋果店交易系統-------")
+    print("  1.設定庫存數量、單價")
+    print("  2.進貨")
+    print("  3.出貨/交易")
+    print("  4.離開系統")
+    print("-------------------------")
+    function=int(input("請輸入欲使用功能選項：")
+    if function==1:
+        myApple=int(input("現在庫存蘋果:"))
+        applePrice=int(input("單價"))
+        print("目前庫存",myApple,"顆 ")
+        print("一顆蘋果",applePrice,"元")
+    elif function==2:
+        applein=int(input("進貨蘋果數量:"))
+        myApple=myApple+applein
+        print("進貨",applein,"顆")
+        print("目前庫存:",myApple,"顆")
+    elif function==3:
+        appleOut=int(input("交易蘋果數量"))
+        total=appleOut * applePrice
+        myApple=myApple-appleOut
+        print("應收金額:",total,"元")
+        print("目前庫存:",myApple,"顆")
+    elif function==4:
+        break
+        
+
+
