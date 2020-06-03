eab2b414f98d24b5d203791505cc6183641276ea
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-06-03 20:10:50 +0800

    Add files via upload

--- 蘋果店.py
+++ 蘋果店.py
@@ -4,20 +4,22 @@ Created on Sat May 30 11:22:22 2020
 
 @author: HARRY LIU
 """
+sellRecord=[]
 while True:
     print("-------蘋果店交易系統-------")
     print("  1.設定庫存數量、單價")
     print("  2.進貨")
     print("  3.出貨/交易")
     print("  4.離開系統")
-    print("-------------------------")
     function=int(input("請輸入欲使用功能選項：")
+    print("----------------------------------")
+   
     if function==1:
         myApple=int(input("現在庫存蘋果:"))
         applePrice=int(input("單價"))
         print("目前庫存",myApple,"顆 ")
         print("一顆蘋果",applePrice,"元")
-    elif function==2:
+    elif function==2: sellRecord.
         applein=int(input("進貨蘋果數量:"))
         myApple=myApple+applein
         print("進貨",applein,"顆")
@@ -28,6 +30,35 @@ while True:
         myApple=myApple-appleOut
         print("應收金額:",total,"元")
         print("目前庫存:",myApple,"顆")
+        sellRecord.append(sellRecord)
+        print("\n目前交易紀錄:")
+        for i in range(len(n)
+            print(input,"賣出",sellRecord[myApple],"顆",appleOut*applePrice,"元")
+elif function==4:
+    print("感謝使用本系統")
+    break
+else:
+    print("請輸入功能1-4")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     elif function==4:
         break
         
