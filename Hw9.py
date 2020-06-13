58ed28b5f5ec3495452794ea1b7587f3c8653cb3
fifi980326 <62453953+fifi980326@users.noreply.github.com>
2020-06-13 09:37:05 +0800

    Add files via upload

---
+++ Hw9.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 09:16:58 2020
+
+@author: HARRY LIU
+"""
+a={}
+while True:
+    key=input
+    if key==0:
+        print(d)
+        break
+    else:
+        if a not in d.pop:
+            value=input("請輸入翻譯")
+            d["a"]=180
+        else:
+        print("此字已經在字典中,請輸入其他的字")
+        
+
